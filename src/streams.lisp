5da72816bff31f4f0952adbbb02134fc762ac57d
pnathan <pnathan@vandals.uidaho.edu>
2013-05-02 23:09:07 -0700

    Got the instance creation and slot handling smoothed out. working on generic functions now

--- src/streams.lisp
+++ src/streams.lisp
@@ -25,25 +25,86 @@
 
 ;;; def!struct doesnt work currently.
 
-;(in-package :cl)
+;; (in-package :cl)
 
 ;;; this is an ad-hoc crappy OO system called: tacky (as opposite of
 ;;; classy, ha, ha, ha).
 
-(defun %deftacky (typename &rest slotnames)
-  (list
-   (cons :typename typename)
-   (cons :slots
-         slotnames)
-   (cons :slot-contents
-         (list (mapcar #'(lambda (slot)
-                           (cons slot nil))
-                       slotnames)))))
+(defparameter *tacky-list* nil)
 
-(defun make-tacky (tacky-class)
-  (lambda (slot &optional value)
-    (if value
-        (set (assoc slot))
-        (cdr (assoc )))))
+(defun deftacky (typename &rest slotnames)
+  (let ((new-object
+         (list
+          (cons :typename typename)
+          (cons :slots
+                slotnames)
+          (cons :slot-contents
+                (list (mapcar #'(lambda (slot)
+                                  (cons slot t))
+                              slotnames))))))
 
-(defun send (object))
+    (cond
+      ;; Already defined
+      ((cdr (assoc typename *tacky-list*))
+       (setf (cdr (assoc typename *tacky-list*)) new-object))
+      ;; New...
+      (t
+       (setf *tacky-list*
+             (acons typename new-object *tacky-list*))))
+    new-object))
+
+(defun make-tacky-instance (class-name)
+  (make-tacky-instance-skeleton
+   (cdr (assoc class-name *tacky-list*))))
+
+(defun make-tacky-instance-skeleton (tacky-class)
+  "Returns an object.
+Messages to the object can either be slotnames or-
+:typename - returns the name of the class"
+  (let ((new-object
+         (copy-tree tacky-class)))
+    #'(lambda (slot &optional value)
+        (cond
+          ((eq slot :typename)
+           (cdr (assoc :typename tacky-class)))
+          (value
+           (setf (cdr (assoc slot (cadr (assoc :slot-contents
+                                              new-object))))
+                 value))
+          (t
+           (cdr (assoc slot (cadr (assoc :slot-contents
+                                        new-object)))))))))
+
+
+(defun slot-reader (object slot)
+  (funcall object slot))
+
+(defun slot-writer (object slot value)
+  (funcall object slot value))
+
+
+(defparameter *generic-function-list* nil
+  "Contains all the known generic functions. Each generic function is
+  actually a dispatching mechanism; a method on a generic function
+  attempts to resolve the object being specified")
+
+(defmacro deftacky-generic (name &rest arglist)
+  `(if (cdr (assoc `,' ,name *generic-function-list*))
+       (setf (cdr (assoc `,' ,name *generic-function-list*))
+             `,' ,arglist)
+       (setf *generic-function-list*
+             (acons `,' ,name `,' ,arglist *generic-function-list*))))
+
+
+(deftacky-generic horse (rider height))
+(deftacky-generic pants (size color))
+
+(defmacro deftacky-method (name args &body body)
+  "Adds body as a lambda function to the generic list"
+  (mapcar #'cdr ,@args)
+  `(lambda (,@(mapcar #'car args)) ,@body ))
+
+(deftacky-method insult-rider ((o horse ))
+  (princ "You moron"))
+
+(defun select-appropriate-method-call (name arglist))
